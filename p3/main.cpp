a8a1f6aed2ebad20825742ccdaf54517871c87b2
temp <archen@w17434.davidson.edu>
2017-10-22 14:55:39 -0400

    Initial Commit

---
+++ p3/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  p3
+//
+//  Created by Chen, Arthur on 10/22/17.
+//  Copyright © 2017 Chen, Arthur. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
